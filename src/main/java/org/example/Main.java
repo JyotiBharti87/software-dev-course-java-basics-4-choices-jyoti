3fa5f8acc9dd4e6a7e87b2f80c265b825e49469b
jyotibharti87 <joybharti87@gmail.com>
2026-05-05 21:10:14 -0500

    final-commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,5 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        CollectionChoiceExercises o1= new CollectionChoiceExercises();
+        o1.MakeColorCollection();
+        o1.MakeEmployeeAccessCodesCollection();
+        o1.MakeStudentCollection();
+        o1.MakeLoggedInUsersCollection();
+
     }
 }
